a9f5613f5c8102d713c427403c13d8e09722a2e8
Martín Rojas <csamuelr.lp@gmail.com>
2025-01-10 16:52:20 -0300

    solucion del proyecto

--- boilerplate-project-headerparser-main/index.js
+++ boilerplate-project-headerparser-main/index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Solution
+app.get('/api/whoami', (req, res) => {
+  let ip = req.ip;
+  let lang = req.headers['accept-language'];
+  let soft = req.headers['user-agent'];
+  res.json({
+    ipaddress: ip,
+    language: lang,
+    software: soft
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
